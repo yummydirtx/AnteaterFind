4bdc9cd8f5633cfbbf9a6d03707a7ba40cf62047
Alex Frutkin <frutkinalexander@gmail.com>
2025-02-21 17:27:27 -0800

    implemented calculate_tf

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ class InvertedIndex:
         file_opener = FileOpener(zipPath)
         self.documents = file_opener.read_zip()
         self.dict = {}
-        #create dict to map {token: posting} - posting might be {doc:tf-idf?}
+        #create dict to map {token: list of postings} - posting might be {doc:tf-idf?}
 
     def tokenize(self, text: str) -> dict:
         """
@@ -17,4 +17,13 @@ class InvertedIndex:
         """
         tokens = re.findall(r'\b[A-Za-z0-9]+\b', text)
         return {token: tokens.count(token) for token in tokens}
+    
+    def calculate_tf(self, token: str, tokens: dict) -> float:
+        """
+        Calculates the term frequency of a token in a document.
+        TF = number of times the token appears in the document / total number
+        of tokens in the document.
+        """
+        # Using or 1 to avoid division by zero
+        return tokens[token] / (sum(tokens.values() or 1))
 
