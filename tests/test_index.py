6c97e5df2daa2c46d52840fdf9dbab7a5159ad38
Alex Frutkin <frutkinalexander@gmail.com>
2025-02-22 14:04:34 -0800

    add test for calculate_tfs method in InvertedIndex

--- tests/test_index.py
+++ tests/test_index.py
@@ -11,3 +11,8 @@ class TestIndex(unittest.TestCase):
         index = InvertedIndex()
         index.documents = {"doc1": "<html><body><p>This is a test.</p></body></html>", "doc2": "<html><body><p>This is only a test.</p></body></html>"}
         self.assertEqual(index.tokenize_documents(), {"doc1": {"thi": 1, "is": 1, "a": 1, "test": 1}, "doc2": {"thi": 1, "is": 1, "a": 1, "test": 1, "onli": 1}})
+
+    def test_calculate_tfs(self):
+        index = InvertedIndex()
+        self.assertEqual(index.calculate_tfs({"thi": 1, "is": 1, "a": 1, "test": 1}), {"thi": 0.25, "is": 0.25, "a": 0.25, "test": 0.25})
+        self.assertEqual(index.calculate_tfs({"thi": 2, "is": 2, "a": 2, "test": 3, "onli": 1}), {"thi": 0.2, "is": 0.2, "a": 0.2, "test": 0.3, "onli": 0.1})
