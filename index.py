4cf641af0d52e1ada0c1fa2b84faa155a50bb18f
Alex Frutkin <frutkinalexander@gmail.com>
2025-02-21 17:17:13 -0800

    added basic static tokenize function to InvertedIndex

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from file import FileOpener
 import math
+import re
 
 # how to find the tf-idf https://www.learndatasci.com/glossary/tf-idf-term-frequency-inverse-document-frequency/
 
@@ -8,6 +9,10 @@ class InvertedIndex:
         self.dict = {}
         #create dict to map {token: posting} - posting might be {doc:tf-idf?}
 
-    def tokenize(self, text):
-        #we use built in tokenize or one our groups
-        pass
+    def tokenize(self, text: str) -> dict:
+        """
+        Tokenizes text into alphanumeric words and returns the frequency of each word.
+        """
+        tokens = re.findall(r'\b[A-Za-z0-9]+\b', text)
+        return {token: tokens.count(token) for token in tokens}
+
