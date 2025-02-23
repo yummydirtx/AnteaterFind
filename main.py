e8655b6cc2080cdf50716db7b9afab0e66d91049
Taravee Tichilasuntron <ttichila@uci.edu>
2025-02-22 20:53:34 -0800

    added developer.zip and generate # of documents

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+from file import FileOpener
 def generate_m1_report():
-    pass
+    openFile = FileOpener(r"zips/developer.zip")
+    readZip = openFile.read_zip()
+    with open("M1Report.txt", 'w') as f:
+        f.write(f"Number of documents: {len(readZip)}\n")
+
+if __name__ == "__main__":
+    generate_m1_report()
