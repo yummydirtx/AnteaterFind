11e805e4f422baead4f964fe5b625fac48dc9599
Taravee Tichilasuntron <ttichila@uci.edu>
2025-02-22 23:28:57 -0800

    create posting format

--- posting.py
+++ posting.py
@@ -1,6 +1,11 @@
 class Posting:
-    def __init__(self, token: str, doc_name: str, tf: float, idf: float):
-        pass
+    def __init__(self,doc_name: str, tf: float, idf: float):
+        """ intialize posting"""
+        self.doc_name = doc_name
+        self.tf = tf
+        self.idf = idf
+        self.tf_idf = tf * idf
 
     def __str__(self) -> str:
-        pass
+        "format for postings"
+        return f"({self.doc_name}, {self.tf_idf})"
