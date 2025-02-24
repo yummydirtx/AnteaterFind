75ae745bc85aff61dd3e907859f8cf5fafa1214b
Alex Frutkin <frutkinalexander@gmail.com>
2025-02-23 17:35:44 -0800

    commented out unused bits for M1 to reduce the index size

--- posting.py
+++ posting.py
@@ -1,5 +1,5 @@
 class Posting:
-    def __init__(self,doc_name: str, tf: float, idf: float):
+    def __init__(self,doc_name: str, tf: float, idf: float = None):
         """ intialize posting"""
         self.doc_name = doc_name
         self.tf = tf
@@ -14,14 +14,14 @@ class Posting:
         """Convert posting to dictionary for JSON serialization"""
         return {
             'doc_name': self.doc_name,
-            'tf': self.tf,
-            'idf': self.idf,
-            'tf_idf': self.tf_idf
+            'tf': self.tf
+            #'idf': self.idf,
+            #'tf_idf': self.tf_idf
         }
 
     @classmethod
     def from_dict(cls, data):
         """Create posting from dictionary"""
-        posting = cls(data['doc_name'], data['tf'], data['idf'])
-        posting.tf_idf = data['tf_idf']
+        posting = cls(data['doc_name'], data['tf'], 0)
+        #posting.tf_idf = data['tf_idf']
         return posting
