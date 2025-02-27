a128d9868f47f2220943bff76d54887846f2cf8d
Alex Frutkin <frutkinalexander@gmail.com>
2025-02-26 21:35:10 -0800

    test: update tokenize_documents test to reflect normalized term frequencies

--- tests/test_index.py
+++ tests/test_index.py
@@ -10,7 +10,7 @@ class TestIndex(unittest.TestCase):
     def test_tokenize_documents(self):
         index = InvertedIndex()
         index.documents = {"doc1": "<html><body><p>This is a test.</p></body></html>", "doc2": "<html><body><p>This is only a test.</p></body></html>"}
-        self.assertEqual(index.tokenize_documents(), {"doc1": {"thi": 1, "is": 1, "a": 1, "test": 1}, "doc2": {"thi": 1, "is": 1, "a": 1, "test": 1, "onli": 1}})
+        self.assertEqual(index.tokenize_documents(), {"doc1": {"thi": 0.25, "is": 0.25, "a": 0.25, "test": 0.25}, "doc2": {"thi": 0.2, "is": 0.2, "a": 0.2, "test": 0.2, "onli": 0.2}})
 
     def test_calculate_tfs(self):
         index = InvertedIndex()
