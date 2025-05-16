c6093c8d2e1d33c87aa59d2c54e3e6513fb25c1f
Alex Frutkin <frutkinalexander@gmail.com>
2025-05-15 22:32:06 -0700

    Refactor summarize function to adjust prompt formatting for improved clarity in content generation.

--- Search/summarizer.py
+++ Search/summarizer.py
@@ -11,10 +11,10 @@ def summarize(text_context, api_key):
     # Truncate text to avoid exceeding context length
     truncated_text = truncate_text(text_context)
     
-    prompt = f"Summarize the following text in 2-3 sentences:\n\n{truncated_text}"
+    prompt = f"\n\n{truncated_text}"
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.0-flash",
-        contents = "You are search engine that provides concise summaries of web pages. Your task is to summarize the content of the following text in 2-3 sentences, no need to start with a complete sentence (aka, no need to clarify that you are summarizing a web page). " + prompt
+        contents = "You are search engine that provides concise summaries of web pages. Your task is to summarize the content of the following text in 2-3 sentences, no need to start with a complete sentence (aka, no need to clarify that you are summarizing a web page). Answer in paragraph form. " + prompt
     )
     return response.text
